91f358e66894b629e3e04c1cfc52d14cc7e147f2
Tobias Fellinger <tobias.fellinger@ages.at>
2024-01-16 13:01:58 +0100

    shinylive now working

--- app/app.R
+++ app/app.R
@@ -16,9 +16,10 @@ library("miniPCH")
 # data --------------------------------------------------------------------
 
 # TODO: set relative to app url automatically
-# url <- "http://127.0.0.1:7446"
-url <- ".."
-datasets <- readRDS(paste0(url, "/datasets.Rds"))
+url <- "http://127.0.0.1:7446"
+# url <- ".."
+# url <- "/usr/share"
+datasets <- readRDS(gzcon(url(paste0(url, "/datasets.Rds"))))
 exclude_from_scenario_vars <- c("recruitment", "random_withdrawal", "n_pat_design")
 filter_scenario_values <- c("recruitment"=0, "random_withdrawal"=0, "n_pat_design"=1000, "method"="logrank")
 scenario_table_vars <- c(c("median_survival_trt", "median_survival_ctrl", "rmst_trt_6m", "rmst_ctrl_6m", "gAHR_6m", "AHR_6m", "rmst_trt_12m", "rmst_ctrl_12m", "gAHR_12m", "AHR_12m", "milestone_survival_trt_6m", "milestone_survival_ctrl_6m", "milestone_survival_trt_12m", "milestone_survival_ctrl_12m"))
